ed295ba68d50cf349e36eb6b8e332b3d0a459ad0
Devanshu <rajdevanshueee@gmail.com>
2021-01-18 20:59:12 +0530

    warnings removed

--- src/containers/Changelogs/index.js
+++ src/containers/Changelogs/index.js
@@ -6,7 +6,7 @@ import Header from '../../components/header';
 import Footer from '../../components/footer';
 import { withGoogleSheets } from 'react-db-google-sheets';
 
-//Images
+//Images      
 import backIcon from '../../assets/images/back-icon.svg';
 import notFound from '../../assets/images/not-found.svg';
 
